be32cee47801f2fe98f271f11228e373f7663252
Tim Frith <tfrith91@gmail.com>
2025-05-06 16:56:47 +0000

    hmmm

--- models/intermediate/int_stock_sales.sql
+++ models/intermediate/int_stock_sales.sql
@@ -5,6 +5,6 @@ select
     forecast_stock,
     stock,
     price
-from {{ref('stg_raw_data_circle__raw_cc_sales')}}
-join {{ref('stg_raw_data_circle__raw_cc_stock')}}
+from {{ref('stg_raw__sales')}}
+join {{ref('stg_raw__stock')}}
 using(product_id)
